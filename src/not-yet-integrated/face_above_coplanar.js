4fe3aa3c1ea0559993af915e5777065fde69b9db
rydealexander <rydealexander@gmail.com>
2019-11-08 22:25:16 +1300

    Updated mesh stuff to run with coplanar faces for face above

--- src/not-yet-integrated/face_above_coplanar.js
+++ src/not-yet-integrated/face_above_coplanar.js
@@ -1,11 +1,15 @@
 // Updated face above method, deals with coplanar faces
 
-import {range} from 'range.js';
+import {range} from '/mesh/utils/range_array.js';
+import {add, multiply, divide, tan, cross, angleBetween, unit} from '@oneisland/math';
 
 // Takes in a face and outputs the points above it, whether the faces surrounding it are coplanar or not
-const pointsAboveCoplanar = (height) => {
+pointsAboveCoplanar(height) {
+  // Define the array that will contain the points above the face
+  const pointsAbove = [];
+
   // Iterate through each vertex in the face
-  for (const vertex of face) {
+  for (const vertex of this.vertices) {
 
     // Define the a variable that is the point that will be returned
     let point = [];
@@ -19,13 +23,13 @@ const pointsAboveCoplanar = (height) => {
     const missingAdjacentIndex = this.adjacentFaces.findIndex(v => v === null);
 
     // Check if there is a missing face
-    if (~missingAdjacentIndex) {
+    if (missingAdjacentIndex) {
 
       // Define which vertex from the edge to use based on which adjacent face is missing (0, 1, 2 == 2, 1, 0)
       const vertexIndex = (missingAdjacentIndex == 0) ? 2 : (missingAdjacentIndex == 2) ? 0 : missingAdjacentIndex;
 
       // Creates a new plane that is parallel to the xz plane and contains one point of the edge
-      planes[1 + missingAdjacentIndex] = new Plane({
+      this.planes[1 + missingAdjacentIndex] = new Plane({
         
         // Calculate the normal
         normal: new Direction([0, 1, 0]), 
@@ -36,27 +40,25 @@ const pointsAboveCoplanar = (height) => {
     }
 
     // Finds one edge that contains the vertex 
-    const edgeNow = face.edges[index];
+    const edgeNow = this.edges[index];
 
     // Finds the other edge that contains the vertex (can be used for a polygon of any number of vertices, providing one edge of the polygon has at most one adjacent face)
-    const edgeBefore = face.edges[((index + (face.vertices.length - 1)) % face.vertices.length)];
+    // Finds the edge before the edge defined above
+    const edgeBefore = this.edges[((index + (this.vertices.length - 1)) % this.vertices.length)];
 
     // Finds the faces that contain edgeNow and edgeBefore
 
-    const faceNow = face.findAdjacentFaceFromEdgeIndex(indexOf(edgeNow));
+    const faceNow = this.findAdjacentFaceFromEdgeIndex(indexOf(edgeNow));
 
-    const faceBefore = face.findAdjacentFaceFromEdgeIndex(indexOf(edgeBefore));
+    const faceBefore = this.findAdjacentFaceFromEdgeIndex(indexOf(edgeBefore));
 
-    // Checks if the three faces (face, faceNow, faceBefore) are parallel, if not computes the normals as per usual
+    // Checks if the three faces (face, faceNow, faceBefore) are not parallel, if that is the case compute the normals as per usual
     if (((cross(faceNow.normal, faceBefore.normal) > epsilon) && ((cross(face.normal, faceBefore.normal) > epsilon) && ((cross(faceNow.normal, face.normal) > epsilon))))) {
 
       // Runs three plane intersection on the three planes
-      point = threePlaneIntersection(face.plane.normal, face.plane.scalar, faceBefore.plane.normal, faceBefore.plane.scalar, faceNow.plane.normal, faceNow.plane.scalar);
-
-      // Add point to the updated face, unsure about how to structure this
+      point = this.plane.pointOfIntersectionWithPlanes(faceBefore.plane, faceNow.plane);
     }
     
-
     // If any two of the three faces are parallel, we try to find three faces that contain the vertex that are not parellel. If this fails, we have a point surrounded by parallel faces,
     // so we return the point plus the height times the normal of the original face, so the point is directly above where it was by a distance of height.
     else {
@@ -65,10 +67,10 @@ const pointsAboveCoplanar = (height) => {
       const containsVertex = [];
 
       // Find all the faces that contain the vertex
-      for (const fac of faces) {
+      for (const fac of mesh.faces) {
 
         // Fac has to be different than the original face
-        if (!fac.equals(face)) {
+        if (!fac.equals(this)) {
 
           // Fac must contain vertex
           if (fac.contains(vertex)) {
@@ -87,7 +89,7 @@ const pointsAboveCoplanar = (height) => {
       // is the number of things we are choosing from and r is the size of the combination. For this case we don't 
       // know how many faces contain the vertex, so we call that n, and we need 3 faces to find the three_plane_intersection,
       // so for this case we need n choose 3. We also only need to find one combination of three faces that are not parallel 
-      // with each other, so we need to break the loop early if thats the case as we're done, and computing cross products is
+      // with each other, so we need to break the loop early if thats the case as we're done, as computing cross products is
       // expensive and we want to avoid doing it.
 
       // An array of the indices of containsVertex
@@ -109,7 +111,7 @@ const pointsAboveCoplanar = (height) => {
 
       // Use each element of listoffirst elements as the first element of the combination
       loop1:
-      for (const element in listOfFirstElements) {
+      for (const element of listOfFirstElements) {
 
         // We need the list of second elements to range from the next element than the current
         // one of listoffirstelements, to the element before the last, so we can save the last at
@@ -118,11 +120,11 @@ const pointsAboveCoplanar = (height) => {
 
         loop2:
         // Use each element of listofsecondelements as the second element of the combination
-        for (const el in listOfSecondElements) {
+        for (const el of listOfSecondElements) {
 
           // We can range the listofthirdelements from the next element from the current element
           // of listofsecondelements to the end, since we only have three elements in our permutations    
-          const listOfThirdElements = array.slice(element + 1);
+          const listOfThirdElements = array.slice(el + 1);
           
           // If element and el meet some condition, in this case that they are not parallel, 
           // move onto the next one in the loop for listofsecondelements
@@ -159,13 +161,13 @@ const pointsAboveCoplanar = (height) => {
 
           loop3:
           // Iterate through the list of third elements to complete our combination
-          for (const l in listOfThirdElements) {
+          for (const l of listOfThirdElements) {
 
-            // If element and l or el and l meet some cElse make result equal the triplet and break out of the whole loopondition move onto the next one in the loop for listofthirdelements
+            // If element and l or el and l meet some condition move onto the next one in the loop for listofthirdelements, else make result equal the triplet and break out of the whole loop
             if ((cross(containsVertex[element].normal, containsVertex[l].normal) < (epsilon, epsilon, epsilon)) || (cross(containsVertex[el].normal, containsVertex[l].normal) < (epsilon, epsilon, epsilon))) { 
               continue;
 
- }
+            }
 
             // Else make result equal the triplet and break out of the whole loop
             else {
@@ -177,28 +179,29 @@ const pointsAboveCoplanar = (height) => {
       }
 
       // If we have a point surrounded by parallel faces (so the loop has run through and hasn't updated result, so result will be '', which is false)
-      // then we add the normal of the face times the height to the point
+      // then we add the normal of the face times the height to the point, so we make the point above directly above it.
       if (!result) {
-        point = add(vertex, multiply(height, unit(face.normal)));
+        point = add(vertex, multiply(height, unit(this.normal)));
       } 
 
       // If we had the case where we have an edge and only two faces to calculate it from, then the point is just the result. We test this by checking the
       // size of the first element in the array result - if it is the case where we only have two different normals the length of the first element will
       // be a single number as it will be the x coordinate, if it has made it through to loop three and updated result with the three different normals
       // it will be a vector of size 3, not just a number.
-      if (result[0].length == 1) {
+      else if (result[0].length == 1) {
         point = result;
       }
 
       // If we have 3 faces that contain the point and are not parallel
       else {
-        point = threePlaneIntersection(result[0].plane.normal, result[0].plane.scalar, result[1].plane.normal, result[1].plane.scalar, result[2].plane.normal, result[2].plane.scalar);
+        point = result[0].plane.pointOfIntersectionWithPlanes(result[1].plane, result[2].plane);
+        
       }
       
 
     }
-
+    pointsAbove.push(point);
   }
-  
-  return point;
+
+  return pointsAbove;
 };
