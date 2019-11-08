77cfff93076aebada55946dadd09b8aeb4fde15c
larkin-nz <daniel@larkin.nz>
2019-11-08 22:55:31 +0800

    Added tooling

---
+++ tools/examples.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Open the examples folder
+cd examples
+
+# Run the cube example
+./cube.js
+
+# Print out the success message
+echo -e "\n\e[32m✔ Successfully ran the cube example \n\e[0m"
+
+# Run the icosahedron example
+./icosahedron.js
+
+# Print out the success message
+echo -e "\n\e[32m✔ Successfully ran the icosahedron example \n\e[0m"
