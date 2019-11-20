ac2c2ba33f95e05615a28a58d35b6667dca97f54
larkin-nz <daniel@larkin.nz>
2019-11-20 11:50:32 +0800

    Patches

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build the mesh library
-../node_modules/.bin/oneisland-babler lib
+node_modules/.bin/oneisland-babler lib
 
 # Print out the success message
 echo -e "\n\e[32m✔ Successfully completed build for mesh library \n\e[0m"
